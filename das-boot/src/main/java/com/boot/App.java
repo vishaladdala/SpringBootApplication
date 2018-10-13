7b6944fee17647af906fc6e607f6331e8775cc20
vishaladdala <vishaladdala@192.168.0.7>
2018-10-12 19:32:49 -0500

    @SpringBootApplication

---
+++ das-boot/src/main/java/com/boot/App.java
@@ -0,0 +1,12 @@
+package com.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
