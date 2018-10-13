b79eccf387aed29713099146f671b6ef408bc36f
vishaladdala <vishaladdala@192.168.0.7>
2018-10-12 19:43:25 -0500

    added springapplication.run

--- das-boot/src/main/java/com/boot/App.java
+++ das-boot/src/main/java/com/boot/App.java
@@ -1,5 +1,6 @@
 package com.boot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -7,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
