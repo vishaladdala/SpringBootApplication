6767e261d2af0bb838404cdb1f162140e2c0946e
vishaladdala <vishaladdala@192.168.0.7>
2018-10-14 19:47:14 -0500

    added services

--- das-boot/src/main/java/com/boot/controller/ShipwreckController.java
+++ das-boot/src/main/java/com/boot/controller/ShipwreckController.java
@@ -2,6 +2,8 @@ package com.boot.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,29 @@ public class ShipwreckController {
 		
 		return ShipwreckStub.list();
 	}
+	
+	@RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
+	public Shipwreck create(@RequestBody Shipwreck shipwreck) {
+		
+		return ShipwreckStub.create(shipwreck);	
+	}
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+	public Shipwreck get(@PathVariable Long id) {
+		return ShipwreckStub.get(id);
+	}
+	
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
+	public Shipwreck update(@PathVariable Long id,@RequestBody Shipwreck shipwreck) {
+		
+		return ShipwreckStub.update(id, shipwreck);
+	}
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
+	public Shipwreck delete(@PathVariable Long id) {
+		return ShipwreckStub.delete(id);
+	}
+	
 
 }
