4fe150f926c4657a00f568f41cb69dd7dbdbdcd1
vishaladdala <vishaladdala@192.168.0.7>
2018-10-14 19:29:20 -0500

    added shipwreck class

---
+++ das-boot/src/main/java/com/boot/controller/ShipwreckController.java
@@ -0,0 +1,18 @@
+package com.boot.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class ShipwreckController {
+	
+	@RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+	public List<Shipwreck> list(){
+		
+	}
+
+}
